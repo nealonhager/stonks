8341e621882ae7fd4fc23b40c72fd9489f0a76b1
Nealon Hager <nealonhager@gmail.com>
2023-02-14 17:15:53 -0800

    load account

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from json.tool import main
-import robin_stocks as rs
+import robin_stocks.robinhood as r
+from dotenv import load_dotenv
+import os
 
 
 def main():
-    print("test")
+    load_dotenv()
+    login = r.login(
+        os.getenv("ROBINHOOD_USERNAME"),
+        os.getenv("ROBINHOOD_PASSWORD")
+    )
 
 
 if __name__ == '__main__':
