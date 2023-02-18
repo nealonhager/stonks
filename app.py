43c935ed4acab0be31fedc6b1d2a97d78ec08973
Nealon Hager <nealonhager@gmail.com>
2023-02-17 18:29:59 -0800

    simulate stock market

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
 from json.tool import main
 import robin_stocks.robinhood as r
+import robin_stocks.robinhood.stocks as stonks
+import robin_stocks.robinhood.orders as orders
+import robin_stocks.robinhood.account as account
 from dotenv import load_dotenv
 import os
+import json
+import schedule
+import time
 
 
-def main():
+def setup():
     load_dotenv()
-    login = r.login(
+    r.login(
         os.getenv("ROBINHOOD_USERNAME"),
         os.getenv("ROBINHOOD_PASSWORD")
     )
+
+
+def teardown():
+    r.logout()
+
+
+def get_buying_power():
+    buying_power = r.profiles.load_account_profile()["cash_balances"]["buying_power"]
+    print(f"buying_power {buying_power}")
+    return buying_power
+
+
+def main():
+    setup()
+    get_buying_power()
+    # schedule.every().minute.do(get_buying_power)
+    
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(10)
+    # orders.order_buy_fractional_by_price(symbol, amountInDollars, timeInForce='gfd', extendedHours=False, jsonify=True)
+    # https://robin-stocks.readthedocs.io/en/latest/robinhood.html#robin_stocks.robinhood.orders.order_buy_fractional_by_price
+    # 'gtc' = good until cancelled. 'gfd' = good for the day. 'ioc' = immediate or cancel. 'opg' execute at opening.
+
+    # teardown()
 
 
 if __name__ == '__main__':
